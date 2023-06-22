7f42c9febf999f0322fb1054420190b49be93751
AFIFFIKRI <afiffikri_94@yahoo.com>
2023-06-22 20:34:21 +0800

    add .env file

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import app from "./app.js";
+import "dotenv/config";
 
 const PORT = 8080;
 
 app.listen(PORT, () => {
-    console.log("Server run on port 8080");
-  });
+  console.log(`Server run on port 8080`);
+});
